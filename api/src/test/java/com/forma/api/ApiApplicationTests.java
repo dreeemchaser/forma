8a1d7f44436f0023125ef900c7983320f6aceb4e
dreamer <StevenAlexanderDraai@gmail.com>
2026-04-24 16:01:56 +0200

    test: configure test environment with H2 and AI stub

--- api/src/test/java/com/forma/api/ApiApplicationTests.java
+++ api/src/test/java/com/forma/api/ApiApplicationTests.java
@@ -1,10 +1,23 @@
 package com.forma.api;
 
+import com.forma.api.dto.CreatePostRequest;
+import com.forma.api.dto.PostAnalysisResponse;
+import com.forma.api.service.PostAnalysisInterface;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootTest
 class ApiApplicationTests {
+
+	@Configuration
+	static class TestConfig {
+		@Bean
+		public PostAnalysisInterface postAnalysisInterface() {
+			return request -> new PostAnalysisResponse(0.0, "Test stub");
+		}
+	}
 
 	@Test
 	void contextLoads() {
